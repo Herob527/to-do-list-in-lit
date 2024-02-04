6293d0f28badcf068cf46e039ab51671253eb3ce
Herob527 <szymon.wrzos@onet.pl>
2024-02-04 01:49:35 +0100

    Removed localStorageEntry

--- src/texts-container.ts
+++ src/texts-container.ts
@@ -2,9 +2,9 @@ import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { repeat } from "lit/directives/repeat.js";
 import "/src/text-input";
-import { type localStorageEntry } from "src/types";
 import { withTwind } from "./utils/twindDecorator";
 import { api } from "./api/localStorageApi";
+import type { Entry } from "./api/type";
 
 @customElement("texts-container")
 @withTwind
@@ -12,7 +12,7 @@ class TextsContainer extends LitElement {
   @property({
     state: true,
   })
-  protected items: localStorageEntry[];
+  protected items: Entry[];
 
   constructor() {
     super();
@@ -30,7 +30,7 @@ class TextsContainer extends LitElement {
   render() {
     return this.items.length > 0
       ? html`<div class="flex flex-col gap-1.5">
-          ${repeat<localStorageEntry>(
+          ${repeat<Entry>(
             this.items,
             (item) => item.id,
             (item) =>
--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export type localStorageEntry = {
-  id: string;
-  value: string;
-};
