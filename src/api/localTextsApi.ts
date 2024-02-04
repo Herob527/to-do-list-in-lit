8f3ab96e58488efcbf756ca82bda733ddf5a6270
Herob527 <szymon.wrzos@onet.pl>
2024-02-04 12:24:34 +0100

    Renaming change

--- src/api/localTextsApi.ts
+++ src/api/localTextsApi.ts
@@ -1,21 +1,21 @@
 import { LOCAL_STORAGE_KEY } from "src/constants";
-import type { Api, Entry } from "./type";
+import type { Api, TextEntry } from "./type";
 import { v4 } from "uuid";
 
-const localStorageApi = () => {
+const localTextsApi = () => {
   if (!localStorage.getItem(LOCAL_STORAGE_KEY)) {
     localStorage.setItem(LOCAL_STORAGE_KEY, "[]");
   }
   return {
     getAll: () =>
-      JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "[]") as Entry[],
+      JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "[]") as TextEntry[],
 
     addNewItem: () => {
-      const dataItem = { id: v4(), value: "" } satisfies Entry;
+      const dataItem = { id: v4(), value: "", category: "None" } satisfies TextEntry;
 
       const currentData = JSON.parse(
         localStorage.getItem(LOCAL_STORAGE_KEY) || "[]",
-      ) as Entry[];
+      ) as TextEntry[];
 
       localStorage.setItem(
         LOCAL_STORAGE_KEY,
@@ -24,25 +24,27 @@ const localStorageApi = () => {
 
       dispatchEvent(new Event("storage"));
     },
-    getItem: (id: string) =>
+    getItem: (id) =>
       JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "[]").find(
-        (e: Entry) => e.id === id,
-      ) as Entry,
-    setItem: (id: string, value: string) => {
-      const entries: Entry[] = JSON.parse(
+        (e: TextEntry) => e.id === id,
+      ) as TextEntry,
+
+    setItem: (id, { value, category }) => {
+      const entries: TextEntry[] = JSON.parse(
         localStorage.getItem(LOCAL_STORAGE_KEY) || "[]",
       );
-      const newEntry = { id, value };
+      const newEntry = { id, value, category };
       const entryIndex = entries.findIndex((entry) => entry.id === id);
       entries[entryIndex] = newEntry;
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify([...entries]));
 
       dispatchEvent(new Event("storage"));
     },
-    deleteItem: (id: string) => {
+
+    deleteItem: (id) => {
       const currentData = JSON.parse(
         localStorage.getItem(LOCAL_STORAGE_KEY) || "[]",
-      ) as Entry[];
+      ) as TextEntry[];
 
       localStorage.setItem(
         LOCAL_STORAGE_KEY,
@@ -51,7 +53,7 @@ const localStorageApi = () => {
 
       dispatchEvent(new Event("storage"));
     },
-  } satisfies Api;
+  } satisfies Api<TextEntry>;
 };
 
-export const api = localStorageApi();
+export const textsApi = localTextsApi();
