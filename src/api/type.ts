fcb26e65efb27999d3fefa2969587fb0ceb0ea79
Herob527 <szymon.wrzos@onet.pl>
2024-02-04 15:15:10 +0100

    Added optional params for adding new item

--- src/api/type.ts
+++ src/api/type.ts
@@ -5,7 +5,7 @@ export interface Entry {
 
 export interface Api<T extends Entry = Entry> {
   getAll(): T[] | Promise<T[]>;
-  addNewItem(): void | Promise<void>;
+  addNewItem(params?: Omit<T, "id">): void | Promise<void>;
   getItem(id: string): T | Promise<T>;
   patchItem(id: string, data: Omit<T, "id">): void | Promise<void>;
   deleteItem(id: string): void | Promise<void>;
