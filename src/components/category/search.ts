8f6081e254aaebe4f87623ef13599b4f0a9c4df8
Herob527 <szymon.wrzos@onet.pl>
2024-02-06 00:36:35 +0100

    Added search by category

---
+++ src/components/category/search.ts
@@ -0,0 +1,52 @@
+import { LitElement, html } from "lit";
+import { customElement, state } from "lit/decorators.js";
+import { withTwind } from "src/utils/twindDecorator";
+import { $filterText } from "src/store/filterText";
+import { categoryApi, type CategoryEntry } from "src/api/localCategoryApi";
+import { repeat } from "lit/directives/repeat.js";
+
+@customElement("category-search")
+@withTwind()
+class CategorySearch extends LitElement {
+  @state()
+  categories: CategoryEntry[] = [];
+
+  constructor() {
+    super();
+    this.categories = categoryApi.getAll();
+
+    window.addEventListener("storage", () => {
+      this.categories = categoryApi.getAll();
+    });
+  }
+  protected render() {
+    return html`
+      <select @change=${this.handleChange} class="p-2 h-full bg-gray-50">
+        ${this.categories.length > 0
+          ? html`<option value="">All</option>
+              ${repeat(
+                this.categories,
+                (item) => item.id,
+                (item) =>
+                  html`<option
+                    value="${item.id}"
+                    ?selected=${item.id === this.id}
+                  >
+                    ${item.value}
+                  </option>`,
+              )}`
+          : html`<option>None</option>`}
+      </select>
+    `;
+  }
+  handleChange(e: Event) {
+    const { value } = e.currentTarget as HTMLInputElement;
+    $filterText.set(value);
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "category-search": CategorySearch;
+  }
+}
