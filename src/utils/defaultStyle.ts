a161b8ff65fdea8d48bb5eaafb609724f360d56d
Herob527 <szymon.wrzos@onet.pl>
2024-02-09 00:22:27 +0100

    Removing redundand packages

--- src/utils/defaultStyle.ts
+++
@@ -1,8 +0,0 @@
-import { css, unsafeCSS, type CSSResultGroup } from "lit";
-import style from "@unocss/reset/tailwind.css?inline";
-
-export const defaultStyle = (customCss: CSSResultGroup | number = 0) => css`
-  ${unsafeCSS(style)};
-  @unocss-placeholder;
-  ${customCss}
-`;
